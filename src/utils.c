1bace49d33b8278a7025e9ee3eb2d79f1b7693ef
Nicolas VON AESCH <nicolas.vonaesch1@gmail.com>
2025-11-13 11:11:51 -0500

    Reformat

--- src/utils.c
+++ src/utils.c
@@ -11,3 +11,12 @@ void addListViewRow(HWND hListView, LPCWSTR col1, LPCWSTR col2){
     ListView_InsertItem(hListView, &item);
     ListView_SetItemText(hListView, item.iItem, 1, (LPWSTR)col2);
 }
+
+void addPEBField(HWND hListView, wchar_t* name, wchar_t* fmt, ...){
+    wchar_t buffer[64];
+    va_list args;
+    va_start(args, fmt);
+    vswprintf(buffer, sizeof(buffer) / sizeof(wchar_t), fmt, args);
+    va_end(args);
+    addListViewRow(hListView, name, buffer);
+}
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,7 @@
 #define UNICODE
 #include <windows.h>
 #include <commctrl.h>
-
+#include <wchar.h>
 
 void addListViewRow(HWND hListView, LPCWSTR col1, LPCWSTR col2);
+void addPEBField(HWND hListView, wchar_t* name, wchar_t* fmt, ...);
